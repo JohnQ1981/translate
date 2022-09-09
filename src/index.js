ae097865dd9d6de64abbd72692be233599d7e210
JohnQ1981 <ikram1981@gmail.com>
2022-09-09 15:21:33 -0500

    added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { faker } from '@faker-js/faker';
 import CommentDetail from './components/CommentDetails';
 const App =() => {
  
@@ -8,37 +7,11 @@ const App =() => {
         <div className="ui container comments">
       
         <CommentDetail />
+        <CommentDetail />
+        <CommentDetail />
+        <CommentDetail />
 
-        <div className="comment">
-          <a href="/" className="avatar">
-            <img alt="avatar" src ={faker.image.avatar()}/>
-          </a>
-          <div className="content">
-            <a href="/" className="author">
-              John
-            </a>
-            <div className="metadata">
-              <span className="date">Today at 5:00PM</span>
-            </div>
-            <div className="text">Nice blog post!</div>
-          </div>
-        </div>
-
-        <div className="comment">
-          <a href="/" className="avatar">
-            <img alt="avatar" src ={faker.image.avatar()}/>
-          </a>
-          <div className="content">
-            <a href="/" className="author">
-              Salih
-            </a>
-            <div className="metadata">
-              <span className="date">Today at 6:00PM</span>
-            </div>
-            <div className="text">Nice blog post!</div>
-          </div>
-        </div>
-
+        
 
       </div>
     
