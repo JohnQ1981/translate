500a322227153b224ec70933a0571b1e98a13b38
JohnQ1981 <ikram1981@gmail.com>
2022-09-09 20:20:13 -0500

    added css

--- src/App.js
+++ src/App.js
@@ -2,36 +2,86 @@ import React from "react";
 import CommentDetail from "./components/CommentDetails";
 import ApprovalCard from "./components/ApprovalCard";
 import { faker } from '@faker-js/faker';
-
+import './App.css';
 const App =() => {
  
     return (
         <div className="ui container comments">
+        {/* <ApprovalCard><div>
+          <h4>Warning!</h4>
+          Are you sure?
+        </div></ApprovalCard> */}
          <ApprovalCard>
-        <CommentDetail 
-        authorName ={faker.name.fullName()} 
-        timeAgo='Today at 5:45 pm' 
-        mnth={faker.date.month()} 
-        imageSrc={faker.image.avatar()} 
-        txt="Nice Blog post!" 
-        city={faker.address.city()}/>
+            <CommentDetail 
+            authorName ={faker.name.fullName()} 
+            timeAgo='Today at 5:45 pm' 
+            mnth={faker.date.month()} 
+            imageSrc={faker.image.avatar()} 
+            authorsMusic={faker.music.songName()}
+            txt="Nice Blog post!" 
+            city={faker.address.city()}
+            authorsAnimal={faker.animal.cat()}     />
+        </ApprovalCard>
+
+        <ApprovalCard>
+            <CommentDetail 
+            authorName ={faker.name.fullName()} 
+            timeAgo='Today at 5:45 pm' 
+            mnth={faker.date.month()} 
+            imageSrc={faker.image.avatar()} 
+            authorsMusic={faker.music.songName()}
+            txt="Nice Blog post!" 
+            city={faker.address.city()}
+            authorsAnimal={faker.animal.cat()}     />
+        </ApprovalCard>
+
+        <ApprovalCard>
+            <CommentDetail 
+            authorName ={faker.name.fullName()} 
+            timeAgo='Today at 5:45 pm' 
+            mnth={faker.date.month()} 
+            imageSrc={faker.image.avatar()} 
+            authorsMusic={faker.music.songName()}
+            txt="Nice Blog post!" 
+            city={faker.address.city()}
+            authorsAnimal={faker.animal.cat()}     />
+        </ApprovalCard>
+
+        <ApprovalCard>
+            <CommentDetail 
+            authorName ={faker.name.fullName()} 
+            timeAgo='Today at 5:45 pm' 
+            mnth={faker.date.month()} 
+            imageSrc={faker.image.avatar()} 
+            authorsMusic={faker.music.songName()}
+            txt="Nice Blog post!" 
+            city={faker.address.city()}
+            authorsAnimal={faker.animal.cat()}     />
         </ApprovalCard>
         
-        <CommentDetail  
-        authorName ={faker.name.fullName()} 
-        timeAgo='Today at 2:45 pm' 
-        mnth={faker.date.month()} 
-        imageSrc={faker.image.avatar()} 
-        txt="Nice Blog post!" 
-        city={faker.address.city()}  />
+        <ApprovalCard>
+            <CommentDetail  
+            authorName ={faker.name.fullName()} 
+            timeAgo='Today at 2:45 pm' 
+            mnth={faker.date.month()} 
+            imageSrc={faker.image.avatar()}
+            authorsMusic={faker.music.songName()} 
+            txt="Nice Blog post!" 
+            city={faker.address.city()}
+            authorsAnimal={faker.animal.cat()}/>
+        </ApprovalCard>
 
-        <CommentDetail  
-        authorName ="Jane" 
-        timeAgo='Today at 1:45 pm' 
-        mnth={faker.date.month()} 
-        imageSrc={faker.image.avatar()} 
-        txt="Nice Blog post!" 
-        city={faker.address.city()} />
+        <ApprovalCard>
+            <CommentDetail  
+            authorName ="Jane" 
+            timeAgo='Today at 1:45 pm' 
+            mnth={faker.date.month()} 
+            imageSrc={faker.image.avatar()}
+            authorsMusic={faker.music.songName()} 
+            txt="Nice Blog post!" 
+            city={faker.address.city()}
+            authorsAnimal={faker.animal.cat()} />
+        </ApprovalCard>
        
 
         
--- src/components/CommentDetails.js
+++ src/components/CommentDetails.js
@@ -1,33 +1,36 @@
 import React from "react";
-import AuthorNames from "./AuthorNames";
-
-
 
 const CommentDetail = (props) => {
-   
-    return (
-
-        <div className="comment">
-        
-          <a href="/" className="avatar">
-            <img alt="avatar" src ={props.imageSrc}/>
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.imageSrc} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.authorName}
+        </a>
+        <div>
+          <a href="/" className="author">
+            Bird:{props.authorsAnimal}{" "}
           </a>
-          <div className="content">
-            <a href="/" className="author">
-            {props.authorName} likes <AuthorNames />
-              
-            </a>
-            <div className="metadata">
-              <span className="date">{props.timeAgo} on {props.mnth}</span>
-            </div>
-            <div className="text">{props.txt}</div>
-            
-            <div>City Auther Lives in : {props.city}</div>
-          </div>
+        </div>
+        <div>
+          <a href="/" className="author">
+            Music:{props.authorsMusic}
+          </a>
         </div>
 
-        
+        <div className="metadata">
+          <span className="date">
+            {props.timeAgo} on {props.mnth}
+          </span>
+        </div>
+        <div className="text">{props.txt}</div>
 
-    );
+        <div>City Author Lives in : {props.city}</div>
+      </div>
+    </div>
+  );
 };
 export default CommentDetail;
